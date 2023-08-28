192c660653e90cecac432932e08b4e8f6bf95bc4
Thiago250801 <thlimabsgim@gmail.com>
2023-08-28 09:44:55 -0400

    backend iniciado

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-console.log("Hello, world");
+const express = require('express');
+const app = express();
 
+app.get("/", function (req, res){
+    res.send("Hello world");
+})
+
+app.listen(3000);
 
 
