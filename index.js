38472cb8667cf03b738550c4e420e99f2c372be4
Thiago250801 <thlimabsgim@gmail.com>
2023-08-29 09:16:28 -0400

    criando os metodos get e post

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ app.get("/oi", function (req, res){
     res.send("Olá mundo!");
 })
 
+
+const herois = ["Mulher Maravilha", "Batman", "Homem Aranha"];
+
+//readAll -> GET
+
+app.get('/herois', function(req, res){
+    res.send(herois);
+})
+
+//create -> POST
+
+app.post('/herois', function (req, res){
+    res.send("Criar registro");
+})
+
 app.listen(3000);
 
 
